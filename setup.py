eb31edc386d2d1829c4c82c46efae5b601745c71
steven.bethard <steven.bethard@localhost>
2009-04-28 15:01:39 +0000

    Update license to Apache 2.0.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 
-# Copyright © 2006-2009 Steven J. Bethard <steven.berthard@gmail.com>.
+# Copyright © 2006-2009 Steven J. Bethard <steven.bethard@gmail.com>.
 #
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted under the terms of the 3-clause BSD
-# license. No warranty expressed or implied.
-# For details, see the accompanying file LICENSE.txt.
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy
+# of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
 
 import textwrap
 import distutils.core
@@ -24,7 +31,7 @@ distutils.core.setup(
         more informative help and usage messages, and much more. At the
         same time, it retains the ease and flexibility of use that made
         optparse so popular."""),
-    license='BSD',
+    license='Apache 2.0',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
